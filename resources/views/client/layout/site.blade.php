78daf7118096a119582b5bbc6ee12ab718679579
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2022-01-11 11:42:26 +0400

    Assets versions fixed

--- resources/views/client/layout/site.blade.php
+++ resources/views/client/layout/site.blade.php
@@ -8,16 +8,16 @@
     />
     <link href="/favicon.ico" rel="shortcut icon">
 
-    <link rel="stylesheet" href="{{ asset('../client/style.css?v=51') }}" />
-    @if(app()->getLocale() === 3) 
+    <link rel="stylesheet" href="{{ asset('../client/style.css?v='.time()) }}" />
+    @if(app()->getLocale() === 3)
     // Language Russia
-    <link rel="stylesheet" href="{{ asset('../client/font_ru.css?v=51') }}" />
+    <link rel="stylesheet" href="{{ asset('../client/font_ru.css?v='.time()) }}" />
     @elseif(app()->getLocale() === 2)
     // Language English
-    <link rel="stylesheet" href="{{ asset('../client/font_en.css?v=51') }}" />
-    @else 
+    <link rel="stylesheet" href="{{ asset('../client/font_en.css?v='.time()) }}" />
+    @else
     // Language Georgia
-    <link rel="stylesheet" href="{{ asset('../client/font_ge.css?v=51') }}" />
+    <link rel="stylesheet" href="{{ asset('../client/font_ge.css?v='.time()) }}" />
     @endif
     @yield('subhead')
 @endsection
@@ -41,8 +41,8 @@
             type="text/javascript"
             src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
     ></script>
-    <script src="{{asset('client/general.js')}}"></script>
-    <script src="{{asset('client/slide.js')}}"></script>
-    <script src="{{asset('client/heroslide.js')}}"></script>
+    <script src="{{asset('client/general.js?v='.time())}}"></script>
+    <script src="{{asset('client/slide.js?v='.time())}}"></script>
+    <script src="{{asset('client/heroslide.js?v='.time())}}"></script>
 @endsection
 
